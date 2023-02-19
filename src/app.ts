8bdd2b82efd7c8c093f32e4849acc06606eb32ad
Ao <vadym.v@aquimarketing.com>
2023-02-19 22:59:58 +0200

    add express session

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,14 @@ import express from 'express';
 import bodyParser from 'body-parser'
 import compression from 'compression';
 import errorHandler from "errorhandler";
+import session from "express-session";
+import * as config from './config'
 
 const app = express();
+
+// Express configuration
+// ...
+
 
 // Gzip responses when appropriate
 app.use(compression());
@@ -12,8 +18,23 @@ app.use(compression());
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}));
 
+app.use(session({
+    secret: config.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: false,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        sameSite: "none",
+        secure: true
+    },
+    store: new session.MemoryStore()
+}));
+
 if (process.env.NODE_ENV === "development") {
     app.use(errorHandler());
 }
+
+
 
 export default app;
--- src/config/express.ts
+++ src/config/express.ts
@@ -1,1 +1,2 @@
 export const PORT = 3000;
+export const COOKIE_SECRET= 'qwertyuiop';
