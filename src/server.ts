a1de1011bb920a5ebab4e9995c3a1d3b82c21e85
Ao <vadym.v@aquimarketing.com>
2023-02-19 22:42:00 +0200

    handle uncaughtException¶

--- src/server.ts
+++ src/server.ts
@@ -15,3 +15,8 @@ if (process.env.NODE_ENV === "development") {
 app.listen(config.PORT, () => {
     console.log(`Server is running on port ${config.PORT}`);
 });
+
+process.on("uncaughtException", function(err) {
+    console.error(err);
+    process.exit(); // exit the process to avoid unknown state
+});
